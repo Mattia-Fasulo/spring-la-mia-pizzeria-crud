317539c37c862de0ca41bb921725eb5522fab434
Mattia-Fasulo <mattia.fasulo@gmail.com>
2023-03-29 17:29:26 +0200

    fix import sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO db_pizzeria.pizzas(description, name, price, img_path) VALUES('marinara', 'pomodoro e origano', 6.50, 'https://www.pizzarecipe.org/wp-content/uploads/2019/01/Pizza-Marinara.jpg');
-INSERT INTO db_pizzeria.pizzas(description, name, price, img_path) VALUES('carrettiera', 'provola, salsiccia e friarielli', 12.00, 'https://th.bing.com/th/id/OIP.jZdD_NDGOkaoga8eZlpO7gHaFk?pid=ImgDet&rs=1');
-INSERT INTO db_pizzeria.pizzas(description, name, price, img_path) VALUES('margherita', 'pomodoro e mozzarella', 8.00, 'https://bing.com/th?id=OSK.a0b6165df951cda8c34d1c7020112703');
-INSERT INTO db_pizzeria.pizzas(description, name, price, img_path) VALUES('atomica', "burrata e 'nduja", 13.00, 'https://th.bing.com/th/id/OIP.3XreRBTz-1szWo-Gv_0x_gHaE8?pid=ImgDet&rs=1');
+INSERT INTO db_pizzeria.pizzas(name, description,  price, img_path) VALUES('carrettiera', 'provola, salsiccia e friarielli', 12.00, 'https://th.bing.com/th/id/OIP.jZdD_NDGOkaoga8eZlpO7gHaFk?pid=ImgDet&rs=1');
+INSERT INTO db_pizzeria.pizzas(name, description,  price, img_path) VALUES('marinara', 'pomodoro e origano', 6.50, 'https://www.pizzarecipe.org/wp-content/uploads/2019/01/Pizza-Marinara.jpg');
+INSERT INTO db_pizzeria.pizzas(name, description,  price, img_path) VALUES('margherita', 'pomodoro e mozzarella', 8.00, 'https://bing.com/th?id=OSK.a0b6165df951cda8c34d1c7020112703');
+INSERT INTO db_pizzeria.pizzas(name, description,  price, img_path) VALUES('atomica', "burrata e 'nduja", 13.00, 'https://th.bing.com/th/id/OIP.3XreRBTz-1szWo-Gv_0x_gHaE8?pid=ImgDet&rs=1');
