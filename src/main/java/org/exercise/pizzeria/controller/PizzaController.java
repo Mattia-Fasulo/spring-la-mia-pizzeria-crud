87eb96588e74bf21cec6860c115fd6ea2404d6c9
Mattia-Fasulo <mattia.fasulo@gmail.com>
2023-03-30 15:21:32 +0200

    added detail view

--- src/main/java/org/exercise/pizzeria/controller/PizzaController.java
+++ src/main/java/org/exercise/pizzeria/controller/PizzaController.java
@@ -3,10 +3,13 @@ package org.exercise.pizzeria.controller;
 import org.exercise.pizzeria.model.Pizza;
 import org.exercise.pizzeria.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -23,5 +26,12 @@ public class PizzaController {
         List<Pizza> pizzas = pizzaRepository.findAll();
         model.addAttribute("list", pizzas);
         return "/pizzas/index";
+    }
+
+    @GetMapping("/{pizzaId}")
+    public String show(Model model, @PathVariable("pizzaId") Integer id ){
+        Pizza p = pizzaRepository.findById(id).orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND));
+        model.addAttribute("pizza", p);
+        return "/pizzas/show";
     }
 }
